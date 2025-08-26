7d232c4bfe9e09fef8e7cb50b35c6638d2c8a7b2
Your Name <conda activate /home/prem/Boonsai/face_swap_video/venv>
2025-08-26 11:03:51 +0530

    artifact and configs added

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -1,50 +1,12 @@
-import os
-from datetime import datetime
-from src.constants import *
+from dataclasses import dataclass
 
-class ConfigEntity:
-    def __init__(self, timestamp = datetime.now()):
-        timestamp = timestamp.strftime("%m_%d_%y__%H_%M_%S")
+@dataclass
+class FaceDetectionArtifact:
+    detected_faces_path: str
+    json_information: str
+    extracted_audio_path: str
 
-        self.face_detection_model = DETECTION_MODEL
-        self.face_swapping_model = SWAPPING_MODEL
-        self.clustering_model = CLUSTERING_MODEL
-        self.audio_model = AUDIO_MODEL
-        self.ctx_id = CTX_ID
-        self.det_size = DET_SIZE
-        
-        self.logs_dir_path = LOG_DIR_PATH
-        self.artifacts_dir_path = ARTIFACT_DIR_PATH
-        self.data_dir_path = DATA_DIR_PATH
-
-class FaceDetectionConfig:
-    def __init__(self, config: ConfigEntity):
-        self.data_dir_path = config.data_dir_path
-        self.artifact_dir_path = config.artifacts_dir_path
-        
-        # face detection main folder
-        self.face_detection_folder_path = os.path.join(
-            self.artifact_dir_path,"face_datection"
-        )
-
-        os.makedirs(self.face_detection_folder_path, exist_ok=True)
-
-        self.detected_faces = os.path.join(
-            self.face_detection_folder_path,"detected_faces" 
-        )
-
-        os.makedirs(self.detected_faces, exist_ok=True)
-
-class FaceSwappingConfig:
-    def __init__(self, config: ConfigEntity):
-        self.data_folder_path = config.data_dir_path
-        self.artifact_folder_path = config.artifacts_dir_path
-
-        self.face_swapped_video_with_audio = os.path.join(
-            self.artifact_folder_path, "face_swapped_video_with_audio"
-        )
-        os.makedirs(self.face_swapped_video_with_audio, exist_ok=True)
-
-
-
-        
+@dataclass
+class FaceSwappingArtifact:
+    final_output_video_path: str
+    
